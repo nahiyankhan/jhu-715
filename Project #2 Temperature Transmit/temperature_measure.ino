1a2f1e054ad80e0c3ca72dc67c0f3de73fc85922
Nahiyan Khan <nahiyan.khan@gmail.com>
2021-02-20 23:50:19 -0500

    measure temperature and transmit over serial

---
+++ Project #2 Temperature Transmit/temperature_measure.ino
@@ -0,0 +1,39 @@
+//TMP36 Pin Variables
+int sensorPin = 0;
+int sensorInput;
+float voltage;
+ 
+/*
+ * setup() - this function runs once when you turn your Arduino on
+ * We initialize the serial connection with the computer
+ */
+void setup()
+{
+  Serial.begin(9600);  //Start the serial connection with the computer
+                       //to view the result open the serial monitor 
+}
+ 
+void loop()                     // run over and over again
+{
+ //getting the voltage reading from the temperature sensor
+ sensorInput = analogRead(sensorPin);  
+ 
+ // converting that reading to voltage, for 3.3v arduino use 3.3
+ voltage = sensorInput * 5;
+ voltage /= 1024.0; 
+ 
+ // print out the voltage
+ Serial.print(voltage); Serial.println(" volts");
+ 
+ // now print out the temperature
+ float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
+                                               //to degrees ((voltage - 500mV) times 100)
+ Serial.print(temperatureC); Serial.println(" degrees C");
+ 
+ // now convert to Fahrenheit
+ float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+ Serial.print(temperatureF); Serial.println(" degrees F");
+
+ // delay by 10 seconds
+ delay(10000);
+}
